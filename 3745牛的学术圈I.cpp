8dad9016642ca5fe4180c225b9e5d378e6bdb92d
junqi1121 <1320208060@qq.com>
2024-03-16 22:08:05 +0800

    3.16 acwing 周赛 并查集！

---
+++ 3745牛的学术圈I.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+const int N = 1e5 + 10, L = 1e5 + 10, C = 1e5 + 10;
+int n, l;
+int c[N];
+bool check(int x)
+{
+	int count = 0, num = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		if (c[i] >= x)
+			count++;
+		if (c[i] == x - 1)
+			num++;
+	}
+	return (count >= x) || (count + num >= x && num <= l);
+}
+int main()
+{
+	cin >> n >> l;
+	for (int i = 1; i <= n; i++)
+		cin >> c[i];
+	int l = 0, r = 1e5 + 10;
+	while (l < r)
+	{
+		int mid = l + r + 1 >> 1;
+		if (check(mid))
+			l = mid;
+		else
+			r = mid - 1;
+	}
+	cout << r;
+	return 0;
+}
---
+++ 5557孤立点数量.cpp
@@ -0,0 +1,55 @@
+// https://www.acwing.com/problem/content/description/5560/
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+using namespace std;
+const int N = 1e5 + 10, M = 1e5 + 10;
+
+int n, m;
+int p[N];
+bool st[N];
+
+int find(int x)
+{
+    // 并查集的路径压缩
+    if (p[x] != x)
+        p[x] = find(p[x]);
+    return p[x];
+}
+
+int main()
+{
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        p[i] = i;
+    }
+
+    for (int i = 1; i <= m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        int pa = find(a), pb = find(b);
+        if (pa == pb)
+        {
+            st[pa] = 1;
+        }
+        else
+        {
+            p[pa] = pb;
+            st[pb] = st[pa] | st[pb];
+        }
+    }
+    int res = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int pi = find(i);
+        if (pi == i && !st[pi])
+        {
+            res++;
+        }
+    }
+    cout << res;
+    return 0;
+}
