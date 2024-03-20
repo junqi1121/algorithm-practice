ae1435a1a1085508aaf94861089660af66279767
junqi1121 <1320208060@qq.com>
2024-03-20 15:09:58 +0800

    lqb 和 模板代码的更新……

--- 蓝桥杯接龙序列.cpp
+++ 蓝桥杯接龙序列.cpp
@@ -18,7 +18,16 @@ typedef long long ll;
 typedef pair<int, int> PII;
 const int N = 1e5 + 10, M = 1e5 + 10;
 int n, m;
+vector<PII> a;
 int h[N], ne[N], e[N], idx;
+unordered_map<int, int> ha;
+int dp[N];
+
+struct NODE
+{
+    int x, y;
+} node[N];
+
 void add(int a, int b)
 {
     e[idx] = b;
@@ -29,7 +38,25 @@ void add(int a, int b)
 int main()
 {
     gkd;
-    cin >> n >> m;
-    cout << n + m;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int x;
+        cin >> x;
+        string y = to_string(x);
+        node[i].x = y[0];
+        node[i].y = y[sz(y) - 1];
+    }
+    int ans = -1;
+    for (int i = 1; i <= n; i++)
+    {
+        dp[i] = ha[node[i].x] + 1;
+        if (dp[i] > ha[node[i].y])
+            ha[node[i].y] = dp[i];
+        ans = max(ans, dp[i]);
+    }
+    cout << n - ans;
+
     return 0;
 }
