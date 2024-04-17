7cf24ec565a230a2f205a4f4bca5211ebbc7451d
junqi1121 <1320208060@qq.com>
2024-04-17 13:35:01 +0800

    不愧是sjtu的题QAQ

--- ac3510sjtu上机.cpp
+++ ac3510sjtu上机.cpp
@@ -15,21 +15,28 @@ inline void solve()
 {
     /* write your code here!! */
     cin >> n;
+    memset(f, 0x3f, sizeof f);
     for (int i = 1; i <= n; i++)
         cin >> a[i], f[a[i]] = i;
     for (int i = 1; i <= n; i++)
         cin >> b[i], b[i] = f[b[i]];
 
     // 现在也就变成了从b中找单增的序列有几个
-    // 想到了转换，但是又不会做上升子序列了QAQ       有O(nlogn)的做法，是二分+贪心的做法！
-    memset(f, 0, sizeof f);
+    // 想到了转换，但是又不会做上升子序列了QAQ          有O(nlogn)的做法，是二分+贪心的做法！
+    // memset(f, 0, sizeof f);
+
     f[1] = b[1];
     int len = 1;
     for (int i = 2; i <= n; i++)
     {
-        if (a[i] > f[len])
+        if (b[i] == inf)
+            continue;
+        // 此处代表做的映射，如果没有出现过，那么就不需要去看这个数了！
+
+        if (b[i] > f[len])
         {
-            f[++len] = a[i];
+            // cout<<i<<' '<<a[i]<<' '<<f[len]<<' '<<len<<endl;
+            f[++len] = b[i];
         }
         else
         {
@@ -37,12 +44,12 @@ inline void solve()
             while (l < r)
             {
                 int mid = l + r + 1 >> 1;
-                if (f[mid] < a[i])
+                if (f[mid] < b[i])
                     l = mid;
                 else
                     r = mid - 1;
             }
-            f[l + 1] = a[i];
+            f[l + 1] = b[i];
         }
     }
     cout << len;
